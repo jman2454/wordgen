9b9ff072ebd14c9b41ff10f5920df3838422010e
James Meyers <james.meyers212@gmail.com>
2019-11-17 15:28:35 -0500

    removed --test flag

--- __main__.py
+++ __main__.py
@@ -6,7 +6,6 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_strings', required=False)
 parser.add_argument('--dataset', required=False)
-parser.add_argument('--test', action='store_true')
 
 args = parser.parse_args()
 
@@ -17,7 +16,7 @@ if (not dataset is None and type(dataset == str)):
         dataset = open(args.dataset, 'r')
         dataset = dataset.read()
 
-if args.test or not args.dataset:
+if not args.dataset:
     dataset = open('wordgen/test.txt', 'r')
     dataset = dataset.read()
 
