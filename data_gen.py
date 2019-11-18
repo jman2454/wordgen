8a63f798c38facc3c46798f1b8c1e2e15269043e
James Meyers <james.meyers212@gmail.com>
2019-11-18 01:47:52 -0500

    optimizations

--- data_gen.py
+++ data_gen.py
@@ -19,9 +19,13 @@ def fill_matrix(dataset):
 
     accepted = list("abcdefghijklmnopqrstuvqxyz") + ['\n']
     for i in range(len(dataset)-1):
-        if (dataset[i+1] in accepted and dataset[i] in accepted):
+        # if (dataset[i+1] in accepted and dataset[i] in accepted):
+        if dataset[i] in accepted:
+            val2 = i+1
+            while (val2 < len(dataset) and not (dataset[val2] in accepted)):
+                val2 += 1
             ind1 = get_ord(dataset[i])
-            ind2 = get_ord(dataset[i+1])
+            ind2 = get_ord(dataset[val2])
             matrix[ind1][ind2] += 1
 
     matrix = preprocessing.normalize(matrix, norm='l1')
